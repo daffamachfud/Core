934a6d8e05e9b86060832401868d8f6b39b6194e
Daffa Radifanka <daffa.radifanka@dretail.id>
2023-09-11 10:21:21 +0700

    Modular phase 1

--- Sources/Core/Extension/CustomError+Ext.swift
+++ Sources/Core/Extension/CustomError+Ext.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 public enum URLError: LocalizedError {
-
+  case invalidRequest
   case invalidResponse
   case addressUnreachable(URL)
 
   public var errorDescription: String? {
     switch self {
+    case .invalidRequest: return "Request is null"
     case .invalidResponse: return "The server responded with garbage."
     case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
     }
